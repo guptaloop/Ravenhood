7408252d9fcca93c887a9c746e2b8a9f4f1acd78
Abhi Gupta <abhifgupta@gmail.com>
2019-07-04 13:18:52 -0700

    add eventlistener and #handleInput to SearchBar

--- frontend/components/search_bar/search_bar.jsx
+++ frontend/components/search_bar/search_bar.jsx
@@ -3,12 +3,27 @@ import React from 'react';
 class SearchBar extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state ={
+			symbol: ''
+		};
+
+	}
+
+	handleInput(type) {
+		return (e) => {
+			this.setState({ [type]: e.target.value });
+		};
 	}
 
 	render() {
 		return (
 			<div className="search-bar">
-				<input type="text" placeholder="search ..." />
+				<input 
+					type="text"
+					placeholder="search ..." 
+					value={this.state.symbol}
+					onChange={this.handleInput('symbol')}
+				/>
 			</div>
 		);
 	}
