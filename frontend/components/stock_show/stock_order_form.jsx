7c3f588e98b158113b2f094340dae8d4a7742c0b
Abhi Gupta <abhifgupta@gmail.com>
2019-07-14 20:19:26 -0700

    update bulk of styling for order form

--- frontend/components/stock_show/stock_order_form.jsx
+++ frontend/components/stock_show/stock_order_form.jsx
@@ -11,9 +11,9 @@ class StockOrderForm extends React.Component {
 		this.getCost = this.getCost.bind(this);
 	}
 
-	componentWillMou() {
+	componentWillMount() {
 		// randomly generate price before component renders
-		this.state.price = (Math.floor(((Math.random() + 0.005) * 1000)));
+		this.state.price = (((Math.random() + 0.005) * 1000)).toFixed(2);
 	}
 
 	handleInput(type) {
@@ -25,7 +25,7 @@ class StockOrderForm extends React.Component {
 	getCost() {
 		const price = this.state.price;
 		const shares = this.state.shares;
-		return (price * shares);
+		return (price * shares).toFixed(2);
 	}
 
 	render() {
@@ -33,36 +33,40 @@ class StockOrderForm extends React.Component {
 
 		return (
 				<form className="order-form">
-					<div className="heading">
+					<div className="heading-div">
 						<h2 className="heading-symbol">{symbol}</h2>
 						{/* will need an eventHandler on this */}
 						<button className="heading-order-type-tog">Buy / Sell</button>
 					</div>
 
-					<div className="order-atts">
-						
+					<div className="order-atts-div">
 						<div className="row">
-							<label>Shares</label>
+							<label className="row-shares">Shares</label>
 							<input
 								type="text"
-								placeholder="full shares only"
-								className="row-shares"
+								
+								className="shares-input"
 								value={this.state.shares}
 								onChange={this.handleInput('shares')}/>
 						</div>
-						
 						<div className="row">
 							<label className="row-price">Market Price</label>
 							<output className="row-output">{this.state.price}</output>
 						</div>
-
+						<div className="price-border"></div>
 						<div className="row">
-							<label>Total Cost</label>
+							<label className="row-value">Total Value</label>
 							<output className="row-output">{this.getCost()}</output>
 						</div>
-
 					</div>
 					
+					<div className="submit-order-button-div">
+						<button className="submit-order-button">Submit Order</button>
+					</div>
+
+					<div className="avail-gold-div">
+						<h5 className="avail-gold">{this.state.price} Gold Available</h5>
+					</div>
 				</form>
 		);
 	}
