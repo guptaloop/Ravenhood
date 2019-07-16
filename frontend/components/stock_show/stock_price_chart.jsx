daac343b64fa786a8e2c06fb4a77a3ca71fe2c75
Abhi Gupta <abhifgupta@gmail.com>
2019-07-15 17:04:14 -0700

    initial commit with working price chart from recharts libray, and create #getHistoricalPrices to seed price chart with date and price data

--- frontend/components/stock_show/stock_price_chart.jsx
+++ frontend/components/stock_show/stock_price_chart.jsx
@@ -1,20 +1,49 @@
 import React from 'react';
+import { ResponsiveContainer, LineChart, Line, XAxis, YAxis } from 'recharts';
 
 class StockPriceChart extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = { 
+			period: 5,
+			prices: []
+		};
+		this.getHistoricalPrices = this.getHistoricalPrices.bind(this);
+	}
+
+	componentWillMount() {
+		this.state.prices = this.getHistoricalPrices();
+	}
+
+	getHistoricalPrices() {
+		let year = 298;
+		let period = this.state.period;
+		let prices = [];
+
+		while (period > 0) {
+			let priceObj = {
+				date: year,
+				price: (((Math.random() + 0.005) * 1000)).toFixed(2)
+			};
+
+			prices.unshift(priceObj);
+			year--;
+			period--;
+		}
+		return prices;
 	}
 
 	render() {
-		return (	
-			<div className="price-chart">
-				<input
-					type="text"
-					className="price-chart-box"
-					placeholder="Price Chart [coming soon]"
-				/>
-			</div>
+		const priceChart = (
+			// <div className="price-chart"></div>
+				<LineChart width={600} height={400} data={this.state.prices}>
+					<XAxis dataKey="date" hide={true} />
+					<YAxis dataKey="price" hide={true} />
+					<Line type="monotone" dataKey="price" stroke="#8884d8" />
+				</LineChart>
 		);
+
+		return (priceChart);
 	}
 }
 
