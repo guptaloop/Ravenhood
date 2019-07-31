d24a18d3ce357a67718f61a4e28b67c8b0691457
Abhi Gupta <abhifgupta@gmail.com>
2019-07-30 20:30:35 -0700

    minor updates to css styling structure for NewsFeed before starting to build out the logic for external news API

--- frontend/components/stock_show/news_feed.jsx
+++ frontend/components/stock_show/news_feed.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
-
+import { isMainThread } from 'worker_threads';
 
 export const NewsFeed = () => {
 
-	
+	// call the fetchNews action from the StockShow container
+	// => pass 'news' in as a prop in StockShow
+	// => add 'news' as deconstructed prop above
+	// => map over it and grab the necessary bits below
+
 	return (
 		<div className="news-feed-div">
 			
 			<h1 className="news-header">News</h1>
 			
-			<div className="news-article">
+			<a href="https://awoiaf.westeros.org/index.php/Tywin_Lannister"
+			className="news-article">
 				<div className="article-text">
-					<h5>The Citadel, date</h5>
-					<a href="https://awoiaf.westeros.org/index.php/Tywin_Lannister">LINK to Article</a>
+					<h5>source, date</h5>
+					<h2>Headline</h2>
 					<h5>ø count</h5>
 				</div>
 				<img className="news-img" src={window.images.mountain_bev} />
-			</div>
-			
-			<div className="news-article">ARTICLE</div>
-			
-			<div className="news-article">ARTICLE</div>
+			</a>
 
 		</div>
-
 	);
 };
