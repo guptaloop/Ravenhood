80dcc9ad924f427afd8f07e9a948ae1565e981c7
Abhi Gupta <abhifgupta@gmail.com>
2019-07-30 20:19:02 -0700

    complete most of the html/css structure for NewsFeed

--- frontend/components/stock_show/news_feed.jsx
+++ frontend/components/stock_show/news_feed.jsx
@@ -6,10 +6,22 @@ export const NewsFeed = () => {
 	
 	return (
 		<div className="news-feed-div">
+			
 			<h1 className="news-header">News</h1>
-			<a href="https://awoiaf.westeros.org/index.php/Tywin_Lannister">ARTICLE</a>
+			
+			<div className="news-article">
+				<div className="article-text">
+					<h5>The Citadel, date</h5>
+					<a href="https://awoiaf.westeros.org/index.php/Tywin_Lannister">LINK to Article</a>
+					<h5>ø count</h5>
+				</div>
+				<img className="news-img" src={window.images.mountain_bev} />
+			</div>
+			
 			<div className="news-article">ARTICLE</div>
+			
 			<div className="news-article">ARTICLE</div>
+
 		</div>
 
 	);
