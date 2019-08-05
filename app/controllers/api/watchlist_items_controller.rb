3dd78323e8d7132bfa4256fb62a409b5daa49f2a
Abhi Gupta <abhifgupta@gmail.com>
2019-08-05 12:36:53 -0700

    create WatchlistItem controller methods -- create and destroy -- and verify changes to db and error/success messages via Postman

--- app/controllers/api/watchlist_items_controller.rb
+++ app/controllers/api/watchlist_items_controller.rb
@@ -3,7 +3,7 @@ class Api::WatchlistItemsController < ApplicationController
 	def create
 		@watchlist_item = WatchlistItem.new(watchlist_item_params)
 		
-		if @watchlist_item.save
+		if @watchlist_item.save!
 			render json: "stock successfully added to watchlist!"
 		else
 			render json: @watchlist_item.errors.full_messages
@@ -11,7 +11,14 @@ class Api::WatchlistItemsController < ApplicationController
 	end
 
 	def destroy
-		
+		@watchlist_item = WatchlistItem.find_by(
+			user_id: params[:user_id], stock_id: params[:stock_id])
+
+		if @watchlist_item.delete
+			render json: "stock successfully removed from watchlist!"
+		else
+			render json: @watchlist_item.errors.full_messages
+		end
 	end
 	
 	private
