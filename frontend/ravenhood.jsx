852ecce009e8abb7d3c1ac368db5c213064463fd
Abhi Gupta <abhifgupta@gmail.com>
2019-08-09 15:52:33 -0700

    create 4 ajax requests to interface with holdings table - verify responses on window

--- frontend/ravenhood.jsx
+++ frontend/ravenhood.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
-// import { addToWatchlist } from './util/stock_api_util';
+import { fetchHoldings, updateHolding, createHolding, destroyHolding } from './util/stock_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
 	const root = document.getElementById('root');
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	window.store = store;
 	delete window.currentUser;
 
-	// window.addToWatchlist = addToWatchlist(2, "LION");
+	window.destroyHolding = destroyHolding(3, 1);
 	
 	ReactDOM.render(<Root store={store}/>, root);
 });
--- frontend/util/stock_api_util.js
+++ frontend/util/stock_api_util.js
@@ -31,9 +31,37 @@ export const addToWatchlist = (user_id, symbol) => (
 	})
 );
 
-export const removeFromWatchlist = (watchlistId, user_id) => (
+export const removeFromWatchlist = (watchlist_id, user_id) => (
 	$.ajax ({
-		url: `/api/stock_watches/${watchlistId}?user_id=${user_id}`,
+		url: `/api/stock_watches/${watchlist_id}?user_id=${user_id}`,
+		method: 'DELETE',
+	})
+);
+
+export const fetchHoldings = (user_id) => (
+	$.ajax ({
+		url: `/api/holdings/${user_id}`,
+		method: 'GET',
+	})
+);
+
+export const updateHolding = (holding_id, user_id, shares) => (
+	$.ajax ({
+		url: `/api/holdings/${holding_id}?user_id=${user_id}&shares=${shares}`,
+		method: 'PATCH',
+	})
+);
+
+export const createHolding = (user_id, symbol, shares, price) => (
+	$.ajax ({
+		url: `/api/holdings?user_id=${user_id}&symbol=${symbol}&shares=${shares}&price=${price}`,
+		method: 'POST',
+	})
+);
+
+export const destroyHolding = (holding_id, user_id) => (
+	$.ajax ({
+		url: `/api/holdings/${holding_id}?user_id=${user_id}`,
 		method: 'DELETE',
 	})
 );
